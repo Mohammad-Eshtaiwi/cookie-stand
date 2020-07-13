36192436dfa0c515e24eb90d818329c50c779b2a
LetMeGuideU <53796803+LetMeGuideU@users.noreply.github.com>
2020-07-13 15:46:46 +0300

    working on table footer

--- js/app.js
+++ js/app.js
@@ -1,248 +1,162 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable quotes */
 "use strict";
-// function GenarateBranches(
-//   minHourlyCustomers,
-//   maxHourlyCustomers,
-//   openingHours = 13,
-//   avgCookieForEachCustomer = 6.3
-// ) {
-//   this.minHourlyCustomers = minHourlyCustomers;
-//   this.maxHourlyCustomers = maxHourlyCustomers;
-//   this.avgCookieForEachCustomer = avgCookieForEachCustomer;
-//   this.customersPerHour = [];
-//   this.avgSalePerHour = [];
-//   this.openingHours = openingHours;
-//   // function to add customers randomly
-//   this.genarateCustomers = function () {
-//     for (let hour = 0; hour < this.openingHours; hour++) {
-//       this.customersPerHour.push(
-//         Math.floor(
-//           Math.random() * (this.maxHourlyCustomers - this.minHourlyCustomers + 1) +
-//             this.minHourlyCustomers
-//         )
-//       );
-//     }
-//   };
-//   //  - function to calculate a avg of the sales
-//   this.calculateAvgSales = function () {
-//     for (let hour = 0; hour < this.openingHours; hour++) {
-//       this.avgSalePerHour.push(
-//         Math.round(
-//           (this.customersPerHour[hour] * this.avgCookieForEachCustomer) /
-//             this.openingHours
-//         )
-//       );
-//     }
-//   };
-// }
-
-const seattle = {
-  openingHours: 14,
-  minHourlyCustomers: 23,
-  maxHourlyCustomers: 65,
-  avgCookieForEachCustomer: 6.3,
-  customersPerHour: [],
-  avgSalePerHour: [],
-  // function to add customers randomly
-  genarateCustomers: function () {
-    for (let hour = 0; hour < this.openingHours; hour++) {
-      this.customersPerHour.push(
-        Math.floor(
-          Math.random() *
-            (this.maxHourlyCustomers - this.minHourlyCustomers + 1) +
-            this.minHourlyCustomers
-        )
-      );
-    }
-  },
-  //  - function to calculate a avg of the sales
-  calculateAvgSales: function () {
-    for (let hour = 0; hour < this.openingHours; hour++) {
-      this.avgSalePerHour.push(
-        Math.round(
-          (this.customersPerHour[hour] * this.avgCookieForEachCustomer) /
-            this.openingHours
-        )
-      );
-    }
-  },
-};
-seattle.genarateCustomers();
-seattle.calculateAvgSales();
-console.log("seattle", seattle);
-const tokyo = {
-  openingHours: 14,
-  minHourlyCustomers: 3,
-  maxHourlyCustomers: 24,
-  avgCookieForEachCustomer: 6.3,
-  customersPerHour: [],
-  avgSalePerHour: [],
-  // function to add customers randomly
-  genarateCustomers: function () {
-    for (let hour = 0; hour < this.openingHours; hour++) {
-      this.customersPerHour.push(
-        Math.floor(
-          Math.random() *
-            (this.maxHourlyCustomers - this.minHourlyCustomers + 1) +
-            this.minHourlyCustomers
-        )
-      );
-    }
-  },
-  //  - function to calculate a avg of the sales
-  calculateAvgSales: function () {
-    for (let hour = 0; hour < this.openingHours; hour++) {
-      this.avgSalePerHour.push(
-        Math.round(
-          (this.customersPerHour[hour] * this.avgCookieForEachCustomer) /
-            this.openingHours
-        )
-      );
-    }
-  },
-};
-tokyo.genarateCustomers();
-tokyo.calculateAvgSales();
-console.log("tokyo", tokyo);
-const dubai = {
-  openingHours: 14,
-  minHourlyCustomers: 11,
-  maxHourlyCustomers: 38,
-  avgCookieForEachCustomer: 6.3,
-  customersPerHour: [],
-  avgSalePerHour: [],
-  // function to add customers randomly
-  genarateCustomers: function () {
-    for (let hour = 0; hour < this.openingHours; hour++) {
-      this.customersPerHour.push(
-        Math.floor(
-          Math.random() *
-            (this.maxHourlyCustomers - this.minHourlyCustomers + 1) +
-            this.minHourlyCustomers
-        )
-      );
-    }
-  },
-  //  - function to calculate a avg of the sales
-  calculateAvgSales: function () {
-    for (let hour = 0; hour < this.openingHours; hour++) {
-      this.avgSalePerHour.push(
-        Math.round(
-          (this.customersPerHour[hour] * this.avgCookieForEachCustomer) /
-            this.openingHours
-        )
-      );
-    }
-  },
-};
-dubai.genarateCustomers();
-dubai.calculateAvgSales();
-console.log("dubai", dubai);
-const paris = {
-  openingHours: 14,
-  minHourlyCustomers: 20,
-  maxHourlyCustomers: 38,
-  avgCookieForEachCustomer: 6.3,
-  customersPerHour: [],
-  avgSalePerHour: [],
-  // function to add customers randomly
-  genarateCustomers: function () {
-    for (let hour = 0; hour < this.openingHours; hour++) {
-      this.customersPerHour.push(
-        Math.floor(
-          Math.random() *
-            (this.maxHourlyCustomers - this.minHourlyCustomers + 1) +
-            this.minHourlyCustomers
-        )
-      );
-    }
-  },
-  //  - function to calculate a avg of the sales
-  calculateAvgSales: function () {
-    for (let hour = 0; hour < this.openingHours; hour++) {
-      this.avgSalePerHour.push(
-        Math.round(
-          (this.customersPerHour[hour] * this.avgCookieForEachCustomer) /
-            this.openingHours
-        )
-      );
-    }
-  },
-};
-paris.genarateCustomers();
-paris.calculateAvgSales();
-console.log("paris", paris);
-const lima = {
-  openingHours: 14,
-  minHourlyCustomers: 2,
-  maxHourlyCustomers: 16,
-  avgCookieForEachCustomer: 6.3,
-  customersPerHour: [],
-  avgSalePerHour: [],
-  // function to add customers randomly
-  genarateCustomers: function () {
-    for (let hour = 0; hour < this.openingHours; hour++) {
-      this.customersPerHour.push(
-        Math.floor(
-          Math.random() *
-            (this.maxHourlyCustomers - this.minHourlyCustomers + 1) +
-            this.minHourlyCustomers
-        )
-      );
-    }
-  },
-  //  - function to calculate a avg of the sales
-  calculateAvgSales: function () {
-    for (let hour = 0; hour < this.openingHours; hour++) {
-      this.avgSalePerHour.push(
-        Math.round(
-          (this.customersPerHour[hour] * this.avgCookieForEachCustomer) /
-            this.openingHours
-        )
-      );
-    }
-  },
-};
-lima.genarateCustomers();
-lima.calculateAvgSales();
-console.log("lima", lima);
-
-function createSalesList(location = {}, name) {
-  const { avgSalePerHour } = location;
-  let hour = 6;
-  let format = "am";
-  let textForLi = [];
-  let article = document.createElement("article");
-  let headding = document.createElement("h2");
-  headding.innerHTML = name;
-  article.appendChild(headding);
-  for (let i = 0; i < avgSalePerHour.length; i++) {
-    textForLi.push(`${hour}${format}: ${avgSalePerHour[i]} cookies`);
-    console.log(hour);
-    if (hour === 11 && format === "am") format = "pm";
-    else if (hour === 11 && format === "pm") format = "am";
-    ++hour;
-    if (hour === 13) hour = 1;
-  }
-  let sum = 0;
-  for (let avg = 0; avg < avgSalePerHour.length; avg++) {
-    sum += avgSalePerHour[avg];
-  }
-  sum = `Total: ${sum}`;
-  textForLi.push(sum);
-  //   textForLi.push()
-  console.log(textForLi);
-  for (let i = 0; i < textForLi.length; i++) {
-    let ul = document.createElement("ul");
-    let list = document.createElement("li");
-    list.innerHTML = textForLi[i];
-    ul.appendChild(list);
-    article.appendChild(ul);
-    document.body.appendChild(article);
-  }
+function GenarateBranches(
+  location,
+  minHourlyCustomers,
+  maxHourlyCustomers,
+  avgCookieForEachCustomer,
+  openingHours = 14,
+  workStartHour = 6
+) {
+  this.location = location;
+  this.minHourlyCustomers = minHourlyCustomers;
+  this.maxHourlyCustomers = maxHourlyCustomers;
+  this.avgCookieForEachCustomer = avgCookieForEachCustomer;
+  this.customersPerHour = [];
+  this.avgSalePerHour = [];
+  this.openingHours = openingHours;
+  this.workStartHour = workStartHour;
+  this.sum = 0;
 }
-createSalesList(seattle, "seattle");
-createSalesList(tokyo, "tokyo");
-createSalesList(dubai, "dubai");
-createSalesList(paris, "paris");
-createSalesList(lima, "lima");
+// - function to calculate a avg of the sales
+GenarateBranches.prototype.genarateCustomers = function () {
+  for (let hour = 0; hour < this.openingHours; hour++) {
+    this.customersPerHour.push(
+      Math.floor(
+        Math.random() *
+          (this.maxHourlyCustomers - this.minHourlyCustomers + 1) +
+          this.minHourlyCustomers
+      )
+    );
+  }
+};
+GenarateBranches.prototype.calculateAvgSales = function () {
+  for (let hour = 0; hour < this.openingHours; hour++) {
+    this.avgSalePerHour.push(
+      Math.round(
+        (this.customersPerHour[hour] * this.avgCookieForEachCustomer) /
+          this.openingHours
+      )
+    );
+  }
+};
+// calculate the sum of the avg sales
+GenarateBranches.prototype.calculateSum = function () {
+  this.sum = sum(this.avgSalePerHour);
+};
+GenarateBranches.prototype.fillTableBody = function () {
+  let tableBody = document.querySelector(".sales-table tbody");
+  console.log(tableBody);
+  let tr = document.createElement("tr");
+  let td = document.createElement("td");
+  tr.appendChild(td);
+  td.innerHTML = this.location;
+  for (let i = 0; i < this.openingHours; i++) {
+    td = document.createElement("td");
+    td.innerHTML = this.avgSalePerHour[i];
+    tr.appendChild(td);
+  }
+  td = document.createElement("td");
+  td.innerHTML = this.sum;
+  tr.appendChild(td);
+  tableBody.appendChild(tr);
+  console.log(tr);
+};
+// make instance of my objects
+const branches = [
+  new GenarateBranches("Seattle", 23, 65, 6.5),
+  new GenarateBranches("Tokyo", 3, 24, 1.2),
+  new GenarateBranches("Dubai", 11, 38, 3.7),
+  new GenarateBranches("paris", 20, 38, 2.3),
+  new GenarateBranches("Lima", 2, 16, 4.6),
+];
+// genarate custumers and
+// calculate the avarage sales per hour for each branch
+branches.forEach((branch) => {
+  branch.genarateCustomers();
+  branch.calculateAvgSales();
+  branch.calculateSum();
+});
+// sum function
+function sum(arr) {
+  let sum = 0;
+  for (let index = 0; index < arr.length; index++) {
+    sum += arr[index];
+  }
+  return sum;
+}
+console.log(branches);
+let openingHours = branches[0].openingHours;
+let openAt = branches[0].workStartHour;
+// function to create a table structure that ready to add data from my objects
+function tableStructure() {
+  let salesTable = document.querySelector(".sales-table");
+  salesTable.innerHTML = `
+    <table border="1">
+    <thead>
+    <tr>
+    </tr>
+    </thead>
+    <tbody></tbody>
+    <tfoot>
+    <tr>
+    </tr>
+    </tfoot>
+  </table>
+  `;
+}
+function tableHeader(workingHours = openingHours, startHour = openAt) {
+  let tableHeader = document.querySelector(".sales-table thead tr");
+  let format = "am";
+  let trTexts = [""];
+  for (let i = 0; i < workingHours; i++) {
+    trTexts.push(`${startHour}:00${format}`);
+    if (startHour === 11 && format === "am") format = "pm";
+    else if (startHour === 11 && format === "pm") format = "am";
+    ++startHour;
+    if (startHour === 13) startHour = 1;
+  }
+
+  trTexts.push("Daily Location Total");
+  console.log(trTexts);
+  console.log(tableHeader);
+  trTexts.map((tr) => {
+    let th = document.createElement("th");
+    th.innerHTML = tr;
+    tableHeader.appendChild(th);
+  });
+}
+function tableFooter(workingHours = openingHours) {
+  let tablefooter = document.querySelector(".sales-table tfoot tr");
+  let th = document.createElement("th");
+  th.innerHTML = "Totals";
+  console.log(th);
+  tablefooter.appendChild(th);
+  for (let hour = 0; hour < openingHours; hour++) {
+    let arr = [];
+    let add = 0;
+    for (let col = 0; col < branches.length; col++) {
+      arr.push(branches[col].avgSalePerHour[hour]);
+    }
+    console.log(arr);
+    add = sum(arr);
+    console.log(add);
+    let td = document.createElement("td");
+    td.innerHTML = add;
+    console.log(td);
+    tablefooter.appendChild(td);
+  }
+  let add = 0;
+  // for (let col = 0; col < openingHours; col++) {
+  //   console.log(branches[col].workStartHour);
+  //   add += branches[col].sum;
+  // }
+  console.log(add);
+}
+tableStructure();
+tableHeader();
+branches.forEach((branch) => {
+  branch.fillTableBody();
+});
+tableFooter();
